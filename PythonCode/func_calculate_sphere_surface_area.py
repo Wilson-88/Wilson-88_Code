37ec1275f4d2131259ee451e86e56b96d67427bb
Wilson-88 <ava_wilson99@163.com>
2024-07-20 15:34:38 +0800

    Create func_calculate_sphere_surface_area.py

---
+++ PythonCode/func_calculate_sphere_surface_area.py
@@ -0,0 +1,7 @@
+# func_calculate_sphere_surface_area.py
+import math
+
+def func_calculate_sphere_surface_area(radius):
+    return 4 * math.pi * radius ** 2
+
+print(func_calculate_sphere_surface_area(3))
