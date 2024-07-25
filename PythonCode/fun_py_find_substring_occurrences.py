db09c7f1cc16e08d08e64ea962b2ea78fbdd0020
Wilson-88 <ava_wilson99@163.com>
2024-07-25 16:32:19 +0800

    Create fun_py_find_substring_occurrences.py

---
+++ PythonCode/fun_py_find_substring_occurrences.py
@@ -0,0 +1,5 @@
+# fun_py_find_substring_occurrences.py
+def fun_py_find_substring_occurrences(string, substring):
+    return string.count(substring)
+
+print(fun_py_find_substring_occurrences("banana", "na"))
