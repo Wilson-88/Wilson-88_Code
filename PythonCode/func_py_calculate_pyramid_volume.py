abebb5827e15d24bf76de27a34db5f5fa5f459b1
Wilson-88 <ava_wilson99@163.com>
2024-07-27 22:13:17 +0800

    Create func_py_calculate_pyramid_volume.py

---
+++ PythonCode/func_py_calculate_pyramid_volume.py
@@ -0,0 +1,5 @@
+# func_py_calculate_pyramid_volume.py
+def func_py_calculate_pyramid_volume(base_area, height):
+    return (1/3) * base_area * height
+
+print(func_py_calculate_pyramid_volume(9, 5))
