4c245c6137f86a8ab819f2251405c759d6c6edb3
Wilson-88 <ava_wilson99@163.com>
2024-08-08 10:26:23 +0800

    Create func_py_calculate_cylinder_lateral_surface_area.py

---
+++ PythonCode/func_py_calculate_cylinder_lateral_surface_area.py
@@ -0,0 +1,7 @@
+# func_py_calculate_cylinder_lateral_surface_area.py
+import math
+
+def func_py_calculate_cylinder_lateral_surface_area(radius, height):
+    return 2 * math.pi * radius * height
+
+print(func_py_calculate_cylinder_lateral_surface_area(3, 5))
