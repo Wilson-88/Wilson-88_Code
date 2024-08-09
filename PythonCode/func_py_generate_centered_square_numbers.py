ba4bbeddcfc595dccca41bcde670b9ecc8b8c0b1
Wilson-88 <ava_wilson99@163.com>
2024-08-09 09:36:46 +0800

    Create func_py_generate_centered_square_numbers.py

---
+++ PythonCode/func_py_generate_centered_square_numbers.py
@@ -0,0 +1,5 @@
+# func_py_generate_centered_square_numbers.py
+def func_py_generate_centered_square_numbers(n):
+    return [i**2 - (i - 1)**2 for i in range(1, n + 1)]
+
+print(func_py_generate_centered_square_numbers(10))
