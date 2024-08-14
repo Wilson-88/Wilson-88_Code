06772c598bc3556f1e0f6c5bd1f738d7b290d9f8
Wilson-88 <ava_wilson99@163.com>
2024-08-14 20:09:08 +0800

    Create func_py_calculate_hexagonal_area.py

---
+++ PythonCode/func_py_calculate_hexagonal_area.py
@@ -0,0 +1,7 @@
+# func_py_calculate_hexagonal_area.py
+import math
+
+def func_py_calculate_hexagonal_area(side_length):
+    return (3 * math.sqrt(3) * side_length ** 2) / 2
+
+print(func_py_calculate_hexagonal_area(4))
