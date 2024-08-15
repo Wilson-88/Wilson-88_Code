711d5bfab5bdfa9807d73d0ea7962864cdda69ab
Wilson-88 <ava_wilson99@163.com>
2024-08-15 21:14:56 +0800

    Create func_py_calculate_octahedron_surface_area.py

---
+++ PythonCode/func_py_calculate_octahedron_surface_area.py
@@ -0,0 +1,7 @@
+# func_py_calculate_octahedron_surface_area.py
+import math
+
+def func_py_calculate_octahedron_surface_area(edge_length):
+    return 2 * math.sqrt(3) * edge_length**2
+
+print(func_py_calculate_octahedron_surface_area(6))
