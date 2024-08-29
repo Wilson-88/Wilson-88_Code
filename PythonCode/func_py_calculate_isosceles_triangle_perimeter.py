2d190b0be1fc2074b519a28be823bc02da6944a6
Wilson-88 <ava_wilson99@163.com>
2024-08-29 14:38:39 +0800

    Create func_py_calculate_isosceles_triangle_perimeter.py

---
+++ PythonCode/func_py_calculate_isosceles_triangle_perimeter.py
@@ -0,0 +1,7 @@
+# func_py_calculate_isosceles_triangle_perimeter.py
+import math
+
+def func_py_calculate_isosceles_triangle_perimeter(base, side):
+    return base + 2 * side
+
+print(func_py_calculate_isosceles_triangle_perimeter(5, 7))
